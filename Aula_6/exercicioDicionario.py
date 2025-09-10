031daa400246a28436815eb3b22a353493948b18
lucas-8844 <carvalho.peres88@gmail.com>
2025-09-10 18:27:11 -0300

    sets

---
+++ Aula_6/exercicioDicionario.py
@@ -0,0 +1,8 @@
+dicionario = {}
+
+dicionario['nome'] = input('Digite seu nome completo: '),
+dicionario['email'] = input('Dgite seu email: '),
+dicionario['Telefone'] = int(input('Digite seu Telefone: ')),
+dicionario['profissao'] = input('Digite sua profissão: '),
+dicionario['email'] = "teste@teste.com"
+print(dicionario)
---
+++ Aula_6/sets.py
@@ -0,0 +1,10 @@
+a = {1,2,3,4,5,6,7}
+b = {4,5,6}
+clientes = ["gabriel", "joão", "gabriel"]
+clientes = set(clientes)
+print(clientes)
+# a.add()
+# a.clear()
+# a.remove()
+# mostrar ainterseção de a e b
+# mostrar a união de a e b
