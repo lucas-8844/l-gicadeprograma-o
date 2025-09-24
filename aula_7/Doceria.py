572c8f836d28dfba0d30a906292952084b987939
lucas-8844 <carvalho.peres88@gmail.com>
2025-09-24 16:13:59 -0300

    atualizaçã0o

--- aula_7/Doceria.py
+++ aula_7/Doceria.py
@@ -2,7 +2,7 @@ import random
 clientes = []
 while True:
     print("="*30)
-    print("Cadastro de clientes")
+    print("Cadastro de clientes".center(30))
     print("="*30)
     print("Deseja fazer um cadastro de um cliente ?")
     opcao = input("Digite sim ou não:")
@@ -13,10 +13,11 @@ while True:
         dicionario['email'] = input("Digite seu email:")
         dicionario['contato'] = input("digite seu contato:")
         clientes.append(dicionario)
+        print("\033[32m Cliente Adicionado Com Sucesso! \033[m")
     elif opcao == "não":
         break
     else:
         print("Digite uma opção válida!")
 clienteAleatorio = random.choice(clientes)
-print(clienteAleatorio)
+print(f"Parabéns {clienteAleatorio["nome"]} você foi sorteado!")
             
