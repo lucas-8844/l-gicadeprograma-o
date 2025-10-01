7d5b04c5cba3d0aee8abb397b210c486f7cdb795
lucas-8844 <carvalho.peres88@gmail.com>
2025-10-01 17:31:03 -0300

    atualização

--- aula_8/gerarSenhas.py
+++ aula_8/gerarSenhas.py
@@ -5,6 +5,42 @@ import string
 def gerar_senha(tamanho=6):
     caracteres = string.ascii_letters + string.digits + string.punctuation
     senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
-    return senha
-print(gerar_senha())  
-print(gerar_senha(12))  
+    senha_gerada = ""
+    senha_gerada += senha
+    return senha_gerada 
+print(gerar_senha(15))
+
+def descobridor_de_senha(senha):
+    contador = 0
+    while True:
+        senha_gerada = ""
+        for i in range(4):
+           senha_gerada += str(random.randint(0,9))
+        contador += 1
+        print(senha_gerada)
+        print(senha)
+        if senha_gerada == senha:
+          print(f"foi descoberto em {contador} tentativas")
+          print(f"acertou a senha é{senha_gerada}")
+        break
+    print(contador)
+
+print(descobridor_de_senha("1234"))
+
+
+
+#def descibridor_senha(senha):
+ #senha_gerada = ""
+ #contador = 0
+ #while senha_gerada != senha:
+    #contador += 1
+    #senha_gerada = ""       
+ #for i in range(4):
+    #senha_gerada += str(random.randint(0,9))
+
+ #if senha_gerada == senha:
+    #print(f"foi descoberto em {senha_gerada} tentativas")
+    #senha_gerada = ""
+   # print(random.randint(0,9))
+
+#print(descibridor_senha("1234"))
